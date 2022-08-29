1c6a6c797c9c43dbece36bfa6cd9f77f9f7dd861
el-pirata-bueno <el-pirata-bueno@yandex.ru>
2022-08-29 11:23:44 +0300

    Коммит №1

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
@@ -1,11 +1,27 @@
 package ru.yandex.practicum.contacts.presentation.sort;
 
 // класс UiState должен иметь package-private доступ
-public class SortUiState {
+class SortUiState {
 
     // сделайте поля isApplyEnable и newSelectedSortType приватными
-    public boolean isApplyEnable = false;
-    public String newSelectedSortType = null;
+    private boolean isApplyEnable = false;
+    private String newSelectedSortType = null;
 
     // реализуйте get и set методы для обоих полей
+    public void setApplyEnable (boolean set) {
+        isApplyEnable = set;
+    }
+
+    public boolean getApplyEnable () {
+        return isApplyEnable;
+    }
+
+    public void setNewSelectedSortType (String set) {
+        newSelectedSortType = set;
+    }
+
+    public String getNewSelectedSortType () {
+        return newSelectedSortType;
+    }
+
 }
